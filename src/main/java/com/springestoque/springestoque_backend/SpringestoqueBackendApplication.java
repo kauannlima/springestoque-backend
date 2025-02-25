556668471b69c98598dd867d85601bb88cd9af2c
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-02-24 23:35:41 -0300

    Primeiro commit

---
+++ src/main/java/com/springestoque/springestoque_backend/SpringestoqueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springestoque.springestoque_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringestoqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringestoqueBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springestoque/springestoque_backend/SpringestoqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springestoque.springestoque_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringestoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
