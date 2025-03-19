f3906d644f8fe29180bcda35adb770b06af5fbf0
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-03-18 23:14:20 -0300

    Adiciona arquivos SQL para inserção de dados iniciais no banco de dados

---
+++ src/main/resources/db/produto.sql
@@ -0,0 +1,7 @@
+-- produto.sql
+-- Inserindo Produtos
+INSERT INTO produtos (nome, categoria_id, descricao, quantidade_em_estoque, fornecedor_id) VALUES
+('Smartphone Samsung', 1, 'Smartphone com 128GB de memória interna', 15, 1),
+('Sofá 3 Lugares', 2, 'Sofá confortável para sala de estar', 12, 2),
+('Mouse Wireless', 1, 'Mouse sem fio com conexão Bluetooth', 30, 1),
+('Cadeira de Escritório', 2, 'Cadeira ergonômica para escritório', 20, 2);
