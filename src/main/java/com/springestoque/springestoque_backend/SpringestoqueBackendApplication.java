f63704fc54d41e4c1c50b249ffb92f686e5bd75b
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-04-03 19:00:09 -0300

    Adicionando diagrama

--- src/main/java/com/springestoque/springestoque_backend/SpringestoqueBackendApplication.java
+++ src/main/java/com/springestoque/springestoque_backend/SpringestoqueBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringestoqueBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringestoqueBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringestoqueBackendApplication.class, args);
+    }
 }
--- src/main/java/com/springestoque/springestoque_backend/config/SecurityConfig.java
+++ src/main/java/com/springestoque/springestoque_backend/config/SecurityConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,19 +14,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()  // Desabilita CSRF se necessário (por exemplo, em APIs com JWT)
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/**").permitAll()  // Permite o acesso a todas as rotas sem autenticação
-                        .anyRequest().authenticated()      // Exige autenticação para outras rotas
+                .csrf(csrf -> csrf.disable()) // Desativa CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()  // Permite acesso ao Swagger sem autenticação
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
     }
 
-    @Bean
+
+@Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
 }
